b5d6671662ac6a453ac5434bf2b796d086b5d7e2
VNEXT\trungvd <trungvd@vnext.com.vn>
2021-03-05 13:55:23 +0700

    change app url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://books-management-env.eba-pztmcypw.ap-southeast-1.elasticbeanstalk.com/api/books/api",
   headers: {
     "Content-type": "application/json"
   }
