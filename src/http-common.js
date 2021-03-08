fbd593080ac2eadef486a121ebcb79540863d91f
VNEXT\trungvd <trungvd@vnext.com.vn>
2021-03-08 11:58:48 +0700

    change rest api endpoint

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://books-management-env.eba-pztmcypw.ap-southeast-1.elasticbeanstalk.com/api/books/api",
+  baseURL: "https://books-management-env.eba-pztmcypw.ap-southeast-1.elasticbeanstalk.com/api/books/api",
   headers: {
     "Content-type": "application/json"
   }
